cc098d3239ce390ad5562000fc16f4456b921198
hyun <kimhyunil2020@gmail.com>
2019-05-09 16:18:55 +0900

    GCP STT&storage upload parser class add

---
+++ Comprehensive_Project/Comprehensive_Project/Google Cloud Platform/SpeechToText.cs
@@ -0,0 +1,42 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+namespace preprocessor.Google_Cloud_Platform
+{
+    class SpeechToText
+    {
+        StringBuilder sttResult;
+        public SpeechToText(String storageUri)
+        {
+            sttResult = new StringBuilder();
+            var speech = SpeechClient.Create();
+            var longOperation = speech.LongRunningRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                //SampleRateHertz = 16000,
+                LanguageCode = "ko",
+            }, RecognitionAudio.FromStorageUri(storageUri));
+            longOperation = longOperation.PollUntilCompleted();
+            var response = longOperation.Result;
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    //Console.WriteLine($"Transcript: { alternative.Transcript}"); //콘솔확인용
+                    sttResult.AppendLine(alternative.Transcript);
+                }
+            }
+        }
+        public String getResult()
+        {
+            return sttResult.ToString();
+        }
+
+    }
+}
---
+++ Comprehensive_Project/Comprehensive_Project/Parser/Parser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moda.Korean.TwitterKoreanProcessorCS;
+namespace preprocessor
+{
+    class Parser
+    {
+        //extractPhraseResult;
+        private String result;
+
+        public Parser(String input)
+        {
+            this.result = this.Tokenize(input);
+        }
+        public String Tokenize(String input)
+        {
+            StringBuilder TokenizeResult = new StringBuilder();
+
+            var tokens = TwitterKoreanProcessorCS.Tokenize(input);
+
+            foreach (var token in tokens)
+            {
+                if ((token.Pos.ToString()).Equals("Noun"))
+                {
+                    TokenizeResult.Append(token.Text + " ");
+                }
+            }
+            return TokenizeResult.ToString();
+        }
+
+        public String getResult()
+        {
+            return result;
+        }
+    }
+}
+

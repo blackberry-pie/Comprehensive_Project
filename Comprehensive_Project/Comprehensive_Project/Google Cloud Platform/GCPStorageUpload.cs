4bd01bf66f83deb7a06cdbfd7a05ba1aa873ba6d
hyun <kimhyunil2020@gmail.com>
2019-05-09 16:01:23 +0900

    nuget package install Google.Cloud.Storage.V1 Google.Apis.Storage.v1 Google.Cloud.Speech.V1 Moda.Korean.TwitterKoreanProcessorCS

---
+++ Comprehensive_Project/Comprehensive_Project/Google Cloud Platform/GCPStorageUpload.cs
@@ -0,0 +1,42 @@
+﻿using Google.Apis.Storage.v1;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+namespace preprocessor.Google_Cloud_Platform
+{
+    //bucketname : speech_limit;
+
+    class GCPStorageUpload
+    {
+        String FileStorageLink;
+        public GCPStorageUpload(string bucketName, string localPath, string objectName = null)
+        {
+            this.UploadFile(bucketName, localPath, objectName);
+        }
+
+        private void UploadFile(string bucketName, string localPath, string objectName = null)
+        {
+            var storage = StorageClient.Create();
+            using (var f = File.OpenRead(localPath))
+            {
+                objectName = objectName ?? Path.GetFileName(localPath);
+                storage.UploadObject(bucketName, objectName, null, f);
+                Console.WriteLine($"Uploaded {objectName}.");
+            }
+            FileStorageLink = "gs://" + bucketName + "/" + objectName;
+        }
+
+        public String getFileStorageLink()
+        {
+            return FileStorageLink;
+        }
+
+    }
+
+}

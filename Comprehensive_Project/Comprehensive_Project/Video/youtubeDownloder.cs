f161a596002a581a3f3824012741d4fc4441ce1f
hyun <kimhyunil2020@gmail.com>
2019-05-13 22:36:55 +0900

    cmd process add

--- Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
+++ Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
@@ -5,15 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Scripting.Hosting;
+using System.Diagnostics;
 
 namespace Comprehensive_Project.Video
 {
     class youtubeDownloder
     {
-
-        public youtubeDownloder()
+        static private string PROCESS_NAME_CMD = "youtubedownloader.exe";
+        static ProcessStartInfo psi = null;
+        static Process proc = null;
+        String resultValue = null;
+        public youtubeDownloder(String link)
         {
-            var engine = Python.CreateEngine();
+            psi = new ProcessStartInfo();
+            psi.FileName = PROCESS_NAME_CMD;
+            psi.CreateNoWindow = false;  // cmd 창 띄우기 -- true(띄우지 않기.) false(띄우기)
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true; // cmd 데이터 받기
+            psi.RedirectStandardInput = true; // cmd 데이터 보내기
+            psi.RedirectStandardError = true; // cmd 오류내용 받기
+
+            proc.StartInfo = psi;
+            proc.Start(); //cmd 시작
+
+
+            proc.StandardInput.Write(link + Environment.NewLine);
+            proc.StandardInput.Close();
+
+            String result = proc.StandardOutput.ReadToEnd();
+
+            proc.WaitForExit();
+            proc.Close();
+
+            resultValue = result;
+
+        }
+
+        public String getResult()
+        {
+            return resultValue;
         }
 
     }

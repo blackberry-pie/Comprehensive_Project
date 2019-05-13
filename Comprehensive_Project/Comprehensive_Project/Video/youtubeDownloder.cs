49ab4b67518751afcc98e8811f56842b84d6848d
hyun <kimhyunil2020@gmail.com>
2019-05-13 23:25:11 +0900

    python 모듈 사용시 오류 수정 요망 다운로더 주석처리함

--- Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
+++ Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
@@ -9,15 +9,18 @@ using System.Diagnostics;
 
 namespace Comprehensive_Project.Video
 {
-    class youtubeDownloder
+    class YoutubeDownloder
     {
         static private string PROCESS_NAME_CMD = "youtubedownloader.exe";
         static ProcessStartInfo psi = null;
         static Process proc = null;
         String resultValue = null;
-        public youtubeDownloder(String link)
+        static String link = null;
+        public YoutubeDownloder(String link)
         {
+            YoutubeDownloder.link = link;
             psi = new ProcessStartInfo();
+            proc = new Process();
             psi.FileName = PROCESS_NAME_CMD;
             psi.CreateNoWindow = false;  // cmd 창 띄우기 -- true(띄우지 않기.) false(띄우기)
             psi.UseShellExecute = false;
@@ -25,21 +28,74 @@ namespace Comprehensive_Project.Video
             psi.RedirectStandardInput = true; // cmd 데이터 보내기
             psi.RedirectStandardError = true; // cmd 오류내용 받기
 
-            proc.StartInfo = psi;
-            proc.Start(); //cmd 시작
+            proc.EnableRaisingEvents = true;
 
 
-            proc.StandardInput.Write(link + Environment.NewLine);
-            proc.StandardInput.Close();
+            proc.ErrorDataReceived += (object sending_process, DataReceivedEventArgs e) =>
+            {
+                if (e.Data != null)
+                {
+                    Console.WriteLine(e.Data.ToString());
+                }
+            };
+            proc.OutputDataReceived += (object sending_process, DataReceivedEventArgs e) =>
+            {
+                if(e.Data != null)
+                {
+                    Console.WriteLine(e.Data.ToString());
+                }
+            };
 
+            DoAction();//cmd 시작
+
+
+
+            /*
             String result = proc.StandardOutput.ReadToEnd();
+
+            Console.WriteLine("result : " + result);
+            resultValue = result;
+            */
+        }
+        static async void DoAction()
+        {
+            proc.StartInfo = psi;
+            proc.Start();
+
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+
+            await DoSignAsync();
+
+            proc.CancelOutputRead();
+            proc.CancelErrorRead();
 
             proc.WaitForExit();
             proc.Close();
 
-            resultValue = result;
+
+
 
         }
+        static async Task<String> DoSignAsync()
+        {
+            await Task.Delay(20);
+
+            proc.StandardInput.Write(link + Environment.NewLine);
+            proc.StandardInput.Close();
+
+            for(int j = 0;j<10 ;j++)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine(j);
+
+            }
+
+
+
+            return null;
+        }
+
 
         public String getResult()
         {

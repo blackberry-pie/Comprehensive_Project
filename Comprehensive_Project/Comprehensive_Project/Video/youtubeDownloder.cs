df27d804c7b319ea787c598bac9b490d6a41afa6
hyun <kimhyunil2020@gmail.com>
2019-05-13 22:14:25 +0900

    IronPython package add

--- Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
+++ Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
@@ -1,13 +1,27 @@
-﻿
+﻿using IronPython.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Scripting.Hosting;
 
 namespace Comprehensive_Project.Video
 {
     class youtubeDownloder
     {
+
+        public youtubeDownloder()
+        {
+            var engine = Python.CreateEngine();
+        }
+
     }
+
+
+
 }
+
+
+
+

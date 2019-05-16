00a49f86971772b58e2ef6d90ae08b1dff3b6b7c
hyun <kimhyunil2020@gmail.com>
2019-05-16 16:17:18 +0900

    youtubedownloader 추가

--- Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
+++ Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
@@ -1,51 +1,27 @@
-﻿using System;
+﻿using IronPython.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Scripting.Hosting;
-using System.Diagnostics;
 
 namespace Comprehensive_Project.Video
 {
-    class YoutubeDownloder
+    class youtubeDownloder
     {
-        static private string PROCESS_NAME_CMD = "youtubedownloader.exe";
-        static ProcessStartInfo psi = null;
-        static Process proc = null;
-        String resultValue = null;
-        public YoutubeDownloder(String link)
+
+        public youtubeDownloder()
         {
-            psi = new ProcessStartInfo();
-            proc = new Process();
-            psi.FileName = PROCESS_NAME_CMD;
-            psi.CreateNoWindow = false;  // cmd 창 띄우기 -- true(띄우지 않기.) false(띄우기)
-            psi.UseShellExecute = false;
-            psi.RedirectStandardOutput = true; // cmd 데이터 받기
-            psi.RedirectStandardInput = true; // cmd 데이터 보내기
-            psi.RedirectStandardError = true; // cmd 오류내용 받기
-
-            proc.StartInfo = psi;
-            proc.Start(); //cmd 시작
-
-
-            proc.StandardInput.Write(link + Environment.NewLine);
-            proc.StandardInput.Close();
-
-            String result = proc.StandardOutput.ReadToEnd();
-
-            proc.WaitForExit();
-            proc.Close();
-
-            resultValue = result;
-
-            Console.WriteLine("(다운로더 클래스)인식결과 출력 : " + result);
-
-        }
-        public String getResult()
-        {
-            return resultValue;
+            var engine = Python.CreateEngine();
         }
 
     }
+
+
+
 }
+
+
+
+

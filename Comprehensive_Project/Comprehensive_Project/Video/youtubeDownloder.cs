ec002e78537f592314fc4b7db4aa66b55ca8f533
hyun <kimhyunil2020@gmail.com>
2019-05-16 22:24:28 +0900

    pytube이용시 추가되는 공백문자 제거

--- Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
+++ Comprehensive_Project/Comprehensive_Project/Video/youtubeDownloder.cs
@@ -11,7 +11,7 @@ namespace Comprehensive_Project.Video
 {
     class YoutubeDownloder
     {
-        static private string PROCESS_NAME_CMD = "youtubedownloader.exe";
+        static readonly private string PROCESS_NAME_CMD = "youtubedownloader.exe";
         static ProcessStartInfo psi = null;
         static Process proc = null;
         String resultValue = null;
@@ -29,16 +29,18 @@ namespace Comprehensive_Project.Video
             proc.EnableRaisingEvents = false;
             proc.StartInfo = psi;
             proc.Start();    //프로세스 시작
-            proc.StandardInput.Write(youtubeLink + Environment.NewLine);     //예를 들어 dir명령어를 입력
+            proc.StandardInput.Write(youtubeLink + Environment.NewLine);     //명령어를 입력
             proc.StandardInput.Close();
             String error = proc.StandardError.ReadToEnd();//오류내용 읽기
 
- 
+
             String resultFileName = proc.StandardOutput.ReadToEnd();
 
-            Console.WriteLine("파일이름(다운로드 클래스 ReadToEnd직후) : " + resultFileName);
+            //Console.WriteLine("파일이름(다운로드 클래스 ReadToEnd직후) : " + resultFileName);
 
-            if(error != null)
+            resultFileName = resultFileName.Replace("", "");// system('cls')로 화면 지울 시 '' 문자가 추가됨 해당 문자 제거
+            
+            if (error != null)
             {
                 Console.WriteLine("error : " + error);
             }

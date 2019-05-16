3e6a676930be2525c83e48f6eab1369dd4d6e5b3
hyun <kimhyunil2020@gmail.com>
2019-05-16 23:12:00 +0900

    filePath 매개변수로 넘길 시 터지는 오류만 해결하면 전처리 완료

--- Comprehensive_Project/Comprehensive_Project/Program.cs
+++ Comprehensive_Project/Comprehensive_Project/Program.cs
@@ -16,31 +16,29 @@ namespace Comprehensive_Project
 
         static int Main(string[] args)
         {
+            /*
             var youtubeLink = Console.ReadLine();
             //youtube 다운로더 모듈 시작
             YoutubeDownloder yd = new YoutubeDownloder(youtubeLink); //args[0]로는 안됨. 직접 값을 넣을 경우 작동함
             var fileName =  yd.getResult();
-            Console.WriteLine("파일 이름 : " + fileName);
+            Console.WriteLine("파일 이름:" + fileName);
             //youtube 다운로더 모듈 종료
 
-            //flac 변환 모듈 시작
-
-
-
-            //flac 변환 모듈 시작
-
-            /*
+            
             //String storageUri = "gs://speech_limit/즉결처형 권한을 가진 나치 장교를 만난 유대인 피아니스트 1ch (online-audio-converter.com).flac"; //google cloud storage link
 
+            */
+            //String objectName = fileName; //"범죄도시中 진선규 조선족 연기ㄷㄷㄷ channel 1(online-audio-converter.com).flac";//구글 스토리지에 업로드되는 이름
+            //String filePath = fileName; //"범죄도시中 진선규 조선족 연기ㄷㄷㄷ channel 1(online-audio-converter.com).flac";//업로드 대상 로컬쪽
 
-            String objectName = fileName; //"범죄도시中 진선규 조선족 연기ㄷㄷㄷ channel 1(online-audio-converter.com).flac";//구글 스토리지에 업로드되는 이름
-            String filePath = fileName; //"범죄도시中 진선규 조선족 연기ㄷㄷㄷ channel 1(online-audio-converter.com).flac";//업로드 대상 로컬쪽
 
+            String objectName = "mono+범죄도시中 진선규 조선족 연기ㄷㄷㄷ.flac";//구글 스토리지에 업로드되는 이름
+            String filePath = "mono+범죄도시中 진선규 조선족 연기ㄷㄷㄷ.flac";//업로드 대상 로컬쪽
 
 
             //STT 모듈 시작
             //GCP Storage upload 모듈
-            GCPStorageUpload storage = new GCPStorageUpload(bucketname, filePath, objectName);
+            var storage = new GCPStorageUpload(bucketname, filePath, objectName);
             String storageUri = storage.getFileStorageLink();
 
 
@@ -58,7 +56,7 @@ namespace Comprehensive_Project
             //Parser 모듈 종료
 
             Console.WriteLine("메인 본문 파서 결과 : " + parserResult + "\n\n\n\n\n\n\n");
-            */
+            
             Console.ReadKey();
             return 0;
 

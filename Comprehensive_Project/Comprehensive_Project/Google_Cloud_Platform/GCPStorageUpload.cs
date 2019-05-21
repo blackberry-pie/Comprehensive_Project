1165a6871124439ba7c7d06513409fd284f08c4d
hyun <kimhyunil2020@gmail.com>
2019-05-21 13:11:10 +0900

    test

--- Comprehensive_Project/Comprehensive_Project/Google_Cloud_Platform/GCPStorageUpload.cs
+++ Comprehensive_Project/Comprehensive_Project/Google_Cloud_Platform/GCPStorageUpload.cs
@@ -17,19 +17,20 @@ namespace Comprehensive_Project.Google_Cloud_Platform
         String FileStorageLink;
         public GCPStorageUpload(string bucketName, string localPath, string objectName = null)
         {
+            Console.WriteLine("localPath : "+ localPath);
             this.UploadFile(bucketName, localPath, objectName);
         }
 
         private void UploadFile(string bucketName, string localPath, string objectName = null)
         {
             var storage = StorageClient.Create();
-            using (var f = File.OpenRead(localPath))
+            using (var f = File.OpenRead(localPath)) // 처리되지 않은 예외: System.ArgumentException: 경로에 잘못된 문자가 있습니다. 오류발생
             {
                 objectName = objectName ?? Path.GetFileName(localPath);
                 storage.UploadObject(bucketName, objectName, null, f);
                 Console.WriteLine($"Uploaded {objectName}.");
             }
-            FileStorageLink = "gs://" + bucketName + "/" + objectName;
+            FileStorageLink = "gs://" + bucketName + "/" + objectName; // 파일 링크 생성
         }
 
         public String getFileStorageLink()

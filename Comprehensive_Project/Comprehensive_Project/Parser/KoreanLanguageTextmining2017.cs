0d510df700fedbcf8229ca5cd2021c92beb79d93
hyun <kimhyunil2020@gmail.com>
2019-05-28 13:41:26 +0900

    파서 교체 준비

--- Comprehensive_Project/Comprehensive_Project/Parser/KoreanLanguageTextmining2017.cs
+++ Comprehensive_Project/Comprehensive_Project/Parser/KoreanLanguageTextmining2017.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace Comprehensive_Project.Parser
 {
     class KoreanLanguageTextmining2017
     {
-        public KoreanLanguageTextmining2017()
+        static readonly private string PROCESS_NAME_CMD = "../../Parser/KLT2010-TestVersion-2017/EXE/indexT.exe";
+        static ProcessStartInfo psi = null;
+        static Process proc = null;
+        String resultValue = null;
+
+        public KoreanLanguageTextmining2017(String input)
         {
 
         }
